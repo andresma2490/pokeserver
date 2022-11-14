485902f6d15d9276d0a24dab356d371fb87a7d0f
andresma <montenegro.andres@correounivalle.edu.co>
2022-11-13 19:43:12 -0500

    initial commit: base setup

---
+++ src/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const url = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((db) => {
+    console.log(`Connected to the database on port ${db.connection.port}`);
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database ", err);
+    process.exit();
+  });
---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+require("./database");
+
+app.use(morgan("dev"));
+app.use(require("./routes/index.routes"));
+
+const PORT = process.env.SERVER_PORT || 8080;
+app.listen(PORT, () => console.log(`server on port ${PORT}`));
